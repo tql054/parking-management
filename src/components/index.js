9f3ef6148f0283b27549fb8030ce0ff6d286afd1
tql054 <tuanquocle2001@gmail.com>
2022-06-20 20:29:48 +0700

    merge

--- src/components/index.js
+++ src/components/index.js
@@ -7,13 +7,10 @@ export {default as Filter} from './filter/Filter'
 export {default as SearchGrid} from './search-grid/SearchGrid'
 export {default as SearchItem} from './search-item/SearchItem'
 export {default as FilterItem} from './filter-item/FilterItem'
-<<<<<<< HEAD
 export {default as Statistical} from './Static/Statistical'
-=======
 export {default as Modal} from './modal/Modal'
 export {default as AreaInfo} from './area-info/AreaInfo'
 
->>>>>>> features/tuan/show-all-odos
 
 
 
