d15a677cb7c3906f13bf3f411bbb694af84c10ad
longbachviet <longbachviet@gmail.com>
2022-06-26 15:50:41 +0700

    tuan

--- src/components/Static/StatisticalMonth.jsx
+++ src/components/Static/StatisticalMonth.jsx
@@ -69,7 +69,6 @@ function StatisticalMonth() {
       switch (filterValue) {
         case "500":
           {
-            console.log(123);
             if (getCate < 500) {
               return item;
             }
@@ -82,40 +81,37 @@ function StatisticalMonth() {
             }
           }
           break;
-        case "5000":
-          {
-            if (getCate < 5000) {
-              return item;
-            }
+        case "5000": {
+          if (getCate < 5000) {
+            return item;
           }
         }
+      }
     });
-        setData(dataFilter);
-        setIsRender(!isRender);
-    }
-
-    function handleSortTime(value) {
-        switch (value) {
-            case "up":
-                data.sort(
-                    (a, b) =>
-                        new Date(a.thoigianbatdau).getTime() -
-                        new Date(b.thoigianbatdau).getTime()
-                );
-                break;
-            case "down":
-                data.sort(
-                    (a, b) =>
-                        new Date(b.thoigianbatdau).getTime() -
-                        new Date(a.thoigianbatdau).getTime()
-                );
-                break;
-        }
-        setIsRender(!isRender);
-    }
-
+    setData(dataFilter);
     setIsRender(!isRender);
-  
+  }
+
+  function handleSortTime(value) {
+    switch (value) {
+      case "up":
+        data.sort(
+          (a, b) =>
+            new Date(a.thoigianbatdau).getTime() -
+            new Date(b.thoigianbatdau).getTime()
+        );
+        break;
+      case "down":
+        data.sort(
+          (a, b) =>
+            new Date(b.thoigianbatdau).getTime() -
+            new Date(a.thoigianbatdau).getTime()
+        );
+        break;
+    }
+    setIsRender(!isRender);
+  }
+
   function handlecars(filterValue) {
     if (!filterValue) {
       fetchData();
@@ -144,105 +140,105 @@ function StatisticalMonth() {
     setIsRender(!isRender);
   }
 
-    const columns = React.useMemo(
-      () => [
-        {
-          Header: "Mã ô đỗ",
-          accessor: "tenodo",
-        },
-        {
-          Header: "Họ và tên",
-          accessor: "hoten", // accessor is the "key" in the data
-        },
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Mã ô đỗ",
+        accessor: "tenodo",
+      },
+      {
+        Header: "Họ và tên",
+        accessor: "hoten", // accessor is the "key" in the data
+      },
 
-        {
-          Header: "Số điện thoại",
-          accessor: "sodienthoai",
-        },
-        {
-          Header: "Biển số xe",
-          accessor: "biensoxe",
-        },
-        {
-          Header: "Loại xe",
-          accessor: "loaixe",
-        },
-        {
-          Header: "Thời gian bắt đầu",
-          accessor: "thoigianbatdau",
-        },
-        {
-          Header: "Thời gian kết thúc",
-          accessor: "thoigianketthuc",
-        },
-        {
-          Header: "Thời gian kết thúc thực",
-          accessor: "thoigiankethucthuc",
-        },
-        {
-          Header: "Trạng thái",
-          accessor: "trangthai",
-        },
-        {
-          Header: "Tổng tiền",
-          accessor: "thanhTien",
-        },
-      ],
-      []
-    );
+      {
+        Header: "Số điện thoại",
+        accessor: "sodienthoai",
+      },
+      {
+        Header: "Biển số xe",
+        accessor: "biensoxe",
+      },
+      {
+        Header: "Loại xe",
+        accessor: "loaixe",
+      },
+      {
+        Header: "Thời gian bắt đầu",
+        accessor: "thoigianbatdau",
+      },
+      {
+        Header: "Thời gian kết thúc",
+        accessor: "thoigianketthuc",
+      },
+      {
+        Header: "Thời gian kết thúc thực",
+        accessor: "thoigiankethucthuc",
+      },
+      {
+        Header: "Trạng thái",
+        accessor: "trangthai",
+      },
+      {
+        Header: "Tổng tiền",
+        accessor: "thanhTien",
+      },
+    ],
+    []
+  );
 
-    return (
-      <div className="wrapper">
-        <div className="combobox">
-          <label for="typeOfCar" className="label loaixe">
-            Loại xe
-          </label>
-          <select
-            name="cars"
-            id="cars"
-            onChange={(e) => handlecars(e.target.value)}
-          >
-            <option value=""> -- Chọn -- </option>
-            <option value="5">5 Chỗ</option>
-            <option value="7">7 chỗ</option>
-          </select>
+  return (
+    <div className="wrapper">
+      <div className="combobox">
+        <label for="typeOfCar" className="label loaixe">
+          Loại xe
+        </label>
+        <select
+          name="cars"
+          id="cars"
+          onChange={(e) => handlecars(e.target.value)}
+        >
+          <option value=""> -- Chọn -- </option>
+          <option value="5">5 Chỗ</option>
+          <option value="7">7 chỗ</option>
+        </select>
 
-          <label for="time">Thời gian :</label>
-          <select
-            name="time"
-            id="time"
-            onChange={(e) => handleSortTime(e.target.value)}
-          >
-            <option value=""> -- Chọn -- </option>
-            <option value="up">Tăng dần</option>
-            <option value="down">Giảm dần</option>
-          </select>
+        <label for="time">Thời gian :</label>
+        <select
+          name="time"
+          id="time"
+          onChange={(e) => handleSortTime(e.target.value)}
+        >
+          <option value=""> -- Chọn -- </option>
+          <option value="up">Tăng dần</option>
+          <option value="down">Giảm dần</option>
+        </select>
 
-          <label for="toatal">Tổng tiền :</label>
-          <select
-            name="total"
-            id="total"
-            onChange={(e) => handlemoney(e.target.value)}
-          >
-            <option value=""> -- Chọn -- </option>
-            <option value="500">Dưới 500.000vnđ</option>
-            <option value="1000">Dưới 1.000.000vnđ</option>
-            <option value="5000">Dưới 5.000.000vnđ</option>
-          </select>
-          <label for="id">Mã ô đỗ :</label>
-          <select
-            name="id"
-            id="id"
-            onChange={(e) => handleSortPlace(e.target.value)}
-          >
-            <option value=""> -- Chọn -- </option>
-            <option value="up">Tăng dần</option>
-            <option value="down">Giảm dần</option>
-          </select>
-        </div>
-
-        <InfoTable columns={columns} data={data} />
+        <label for="toatal">Tổng tiền :</label>
+        <select
+          name="total"
+          id="total"
+          onChange={(e) => handlemoney(e.target.value)}
+        >
+          <option value=""> -- Chọn -- </option>
+          <option value="500">Dưới 500.000vnđ</option>
+          <option value="1000">Dưới 1.000.000vnđ</option>
+          <option value="5000">Dưới 5.000.000vnđ</option>
+        </select>
+        <label for="id">Mã ô đỗ :</label>
+        <select
+          name="id"
+          id="id"
+          onChange={(e) => handleSortPlace(e.target.value)}
+        >
+          <option value=""> -- Chọn -- </option>
+          <option value="up">Tăng dần</option>
+          <option value="down">Giảm dần</option>
+        </select>
       </div>
-    );
-  }
+
+      <InfoTable columns={columns} data={data} />
+    </div>
+  );
+}
 export default StatisticalMonth;
