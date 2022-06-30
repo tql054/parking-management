d5576222b7a7cb33afb76024ea3aede1dd04abb5
longbachviet <longbachviet@gmail.com>
2022-06-30 10:35:50 +0700

    final

--- src/components/Static/TableData.jsx
+++ src/components/Static/TableData.jsx
@@ -110,10 +110,6 @@ const InfoTable = ({ columns, data }) => {
             </tbody>
           </table>
           <div class="gotoPage">
-            <div className="thongKe">
-              <span>Tổng tiền là :</span>
-            </div>
-
             <div className="gotoPage_item">
               <span id="page_number">
                 page{""}
